d2652524dafba651f37e6108f480fcbaad0f243d
tysky <dtysky51@gmail.com>
2017-10-03 13:45:50 +0300

    add tests

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -1,20 +1,56 @@
+import fs from 'mz/fs';
+import path from 'path';
+import os from 'os';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
 import nock from 'nock';
-// import axios from 'axios';
-// import httpAdapter from 'axios/lib/adapters/http';
 import pageLoader from '../src/index';
 
-const host = 'http://localhost';
+axios.defaults.adapter = httpAdapter;
+
+const host = 'http://example.com';
 const body = 'hello world';
 
+beforeEach(() => {
+  nock(host)
+    .get('/')
+    .reply(200, body);
+});
 
-nock(host)
-  .get('/')
-  .reply(200, body);
-
-test('test web page loader', () => {
+test('test simple get response status', () => {
   expect.assertions(1);
-  return pageLoader(host)
+  return axios.get(host)
+    .then((response) => {
+      expect(response.status).toBe(200);
+    });
+});
+
+test('test simple get response body', () => {
+  expect.assertions(1);
+  return axios.get(host)
     .then((response) => {
       expect(response.data).toBe(body);
     });
 });
+
+test('test downloading page', () => {
+  const tempDirPath = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+  expect.assertions(1);
+  return pageLoader(host, tempDirPath)
+    .then(() => fs.readFile(path.join(tempDirPath, 'example-com.html')))
+    .then(fileData => expect(fileData.toString()).toBe(body));
+});
+// test('test downloading page', () => {
+//   expect.assertions(1);
+//   return fs.mkdtemp(`${os.tmpdir()}${path.sep}`)
+//     .then((tempDirPath) => {
+//       pageLoader(host, tempDirPath);
+//       console.log('++++++', tempDirPath);
+//       return tempDirPath;
+//     })
+//     .then((tempDirPath) => {
+//       console.log('------', tempDirPath);
+//       fs.readFile(path.join(tempDirPath, 'example-com.html'));
+//     })
+//     .then(fileData => expect(fileData.toString()).toBe(body));
+// });
