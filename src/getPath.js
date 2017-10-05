ef0068a4c6292c35bf2dc4d31a679a0b115f0884
tysky <dtysky51@gmail.com>
2017-10-05 22:21:13 +0300

    refactor

--- src/getPath.js
+++ src/getPath.js
@@ -1,24 +1,26 @@
 import url from 'url';
-import pathNode from 'path';
+import path from 'path';
 
 const replaceSymbols = str => str.replace(/\W/g, '-');
 
 const getPath = (urlName, dirName, suffix) => {
-  const { host, path } = url.parse(urlName);
-  const correctPath = path.slice(-1) === '/' ? path.slice(0, -1) : path;
+  // const { host, path } = url.parse(urlName);
+  const { host } = url.parse(urlName);
+  const pathUrl = url.parse(urlName).path;
+  const correctPath = pathUrl.slice(-1) === '/' ? pathUrl.slice(0, -1) : pathUrl;
   const fileName = `${replaceSymbols(host)}${replaceSymbols(correctPath)}${suffix}`;
-  return pathNode.resolve(dirName, fileName);
+  return path.resolve(dirName, fileName);
 };
 
 export const getPagePath = (urlName, outputDir) => getPath(urlName, outputDir, '.html');
 export const getSrcDirPath = (urlName, outputDir) => getPath(urlName, outputDir, '_files');
 export const getSrcFilePath = (srcDirPath, srcName) => {
-  const { dir, name, ext } = pathNode.parse(srcName.slice(1));
+  const { dir, name, ext } = path.parse(srcName.slice(1));
   const srcFileName = `${replaceSymbols(dir)}-${replaceSymbols(name)}${ext}`;
-  return pathNode.resolve(srcDirPath, srcFileName);
+  return path.resolve(srcDirPath, srcFileName);
 };
 export const getSrcLocalPath = (srcDirPath, srcName) => {
-  const { dir, base } = pathNode.parse(getSrcFilePath(srcDirPath, srcName));
-  const { name } = pathNode.parse(dir);
+  const { dir, base } = path.parse(getSrcFilePath(srcDirPath, srcName));
+  const { name } = path.parse(dir);
   return `${name}/${base}`;
 };
