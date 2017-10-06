072bb5323f5596aa9de30576014732a5857c5a5e
tysky <dtysky51@gmail.com>
2017-10-06 14:25:36 +0300

    add tests for errors

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -10,56 +10,72 @@ const fixturesSrcPath = './__tests__/__fixtures__/src';
 const testPagePath = path.join(__dirname, '/__fixtures__/example.html');
 const host = 'http://example.com';
 
-beforeEach(() => {
-  nock(host)
-    .get('/')
-    .replyWithFile(200, path.join(__dirname, '/__fixtures__/example.html'))
-    .get('/src/app.css')
-    .replyWithFile(200, path.join(__dirname, '/__fixtures__/src/app.css'))
-    .get('/src/app.js')
-    .replyWithFile(200, path.join(__dirname, '/__fixtures__/src/app.js'))
-    .get('/src/cat.jpg')
-    .replyWithFile(200, path.join(__dirname, '/__fixtures__/src/cat.jpg'));
+describe('Test success requests', () => {
+  beforeEach(() => {
+    nock(host)
+      .get('/')
+      .replyWithFile(200, path.join(__dirname, '/__fixtures__/example.html'))
+      .get('/src/app.css')
+      .replyWithFile(200, path.join(__dirname, '/__fixtures__/src/app.css'))
+      .get('/src/app.js')
+      .replyWithFile(200, path.join(__dirname, '/__fixtures__/src/app.js'))
+      .get('/src/cat.jpg')
+      .replyWithFile(200, path.join(__dirname, '/__fixtures__/src/cat.jpg'));
+  });
+
+  test('test simple get response status', () => {
+    expect.assertions(1);
+    return expect(axios.get(host)
+      .then(response => response.status)).resolves.toBe(200);
+  });
+
+  test('test simple get response body', () => {
+    const body = fs.readFileSync(testPagePath);
+    expect.assertions(1);
+    return expect(axios.get(host)
+      .then(response => response.data.toString())).resolves.toBe(body.toString());
+  });
+
+  test('sources download test', () => {
+    const tempDirPath = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+
+    const cssPath = path.join(fixturesSrcPath, 'app.css');
+    const jsPath = path.join(fixturesSrcPath, 'app.js');
+    const imgPath = path.join(fixturesSrcPath, 'cat.jpg');
+    const expectedCSS = fs.readFileSync(cssPath);
+    const expectedJS = fs.readFileSync(jsPath);
+    const expectedIMG = fs.readFileSync(imgPath);
+
+    const testFilesPath = 'example-com_files';
+    expect.assertions(3);
+    return pageLoader(host, tempDirPath)
+      .then(() => fs.readFile(path.join(tempDirPath, testFilesPath, 'src-app.css')))
+      .then(cssFile => expect(cssFile.toString()).toBe(expectedCSS.toString()))
+      .then(() => fs.readFile(path.join(tempDirPath, testFilesPath, 'src-app.js')))
+      .then(jsFile => expect(jsFile.toString()).toBe(expectedJS.toString()))
+      .then(() => fs.readFile(path.join(tempDirPath, testFilesPath, 'src-cat.jpg')))
+      .then(imgFile => expect(imgFile.toString()).toBe(expectedIMG.toString()));
+  });
 });
 
-test('test simple get response status', () => {
-  expect.assertions(1);
-  return expect(axios.get(host)
-    .then(response => response.status)).resolves.toBe(200);
-  // return axios.get(host)
-  //   .then((response) => {
-  //     expect(response.status).toBe(200);
-  //   });
-});
-
-test('test simple get response body', () => {
-  const body = fs.readFileSync(testPagePath);
-  expect.assertions(1);
-  return expect(axios.get(host)
-    .then(response => response.data.toString())).resolves.toBe(body.toString());
-  // return axios.get(host)
-  //   .then((response) => {
-  //     expect(response.data.toString()).toBe(body.toString());
-  //   });
-});
-
-test('sources download test', () => {
-  const tempDirPath = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
-
-  const cssPath = path.join(fixturesSrcPath, 'app.css');
-  const jsPath = path.join(fixturesSrcPath, 'app.js');
-  const imgPath = path.join(fixturesSrcPath, 'cat.jpg');
-  const expectedCSS = fs.readFileSync(cssPath);
-  const expectedJS = fs.readFileSync(jsPath);
-  const expectedIMG = fs.readFileSync(imgPath);
-
-  const testFilesPath = 'example-com_files';
-  expect.assertions(3);
-  return pageLoader(host, tempDirPath)
-    .then(() => fs.readFile(path.join(tempDirPath, testFilesPath, 'src-app.css')))
-    .then(cssFile => expect(cssFile.toString()).toBe(expectedCSS.toString()))
-    .then(() => fs.readFile(path.join(tempDirPath, testFilesPath, 'src-app.js')))
-    .then(jsFile => expect(jsFile.toString()).toBe(expectedJS.toString()))
-    .then(() => fs.readFile(path.join(tempDirPath, testFilesPath, 'src-cat.jpg')))
-    .then(imgFile => expect(imgFile.toString()).toBe(expectedIMG.toString()));
+describe('Test errors', () => {
+  test('404 page test', () => {
+    nock(host).get('/404page').reply(404);
+    const tempDirPath = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+    expect.assertions(1);
+    return expect(pageLoader(`${host}/404page`, tempDirPath))
+      .rejects.toMatch('Error 404. Page not found');
+  });
+  test('test downloading to nonexistent directory', () => {
+    nock(host).get('/').reply(200, 'hello world');
+    expect.assertions(1);
+    return expect(pageLoader(host, './nonexistent'))
+      .rejects.toMatch("Error. The directory doesn't exists");
+  });
+  test('passed filepath instead of path to the directory', () => {
+    nock(host).get('/').reply(200, 'hello world');
+    expect.assertions(1);
+    return expect(pageLoader(host, './__fixtures__/file.md'))
+      .rejects.toMatch("Error. Given path isn't path to the directory");
+  });
 });
