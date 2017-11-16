1c34a47d553d4c6ac61dbbffacdd24f85f9c1c4a
tysky <dtysky51@gmail.com>
2017-11-16 21:18:27 +0300

    update loader to async/await

--- src/loader.js
+++ src/loader.js
@@ -16,9 +16,9 @@ const loadFiles = (pageUrl, links, srcDirPath) => {
     const tasks = new Listr([
       {
         title: srcUrl,
-        task: () => {
-          axios.get(srcUrl, { responseType: 'arraybuffer' })
-            .then(response => fs.writeFile(getSrcFilePath(srcDirPath, link), response.data));
+        task: async () => {
+          const response = await axios.get(srcUrl, { responseType: 'arraybuffer' });
+          await fs.writeFile(getSrcFilePath(srcDirPath, link), response.data);
         },
       },
     ]);
@@ -26,44 +26,39 @@ const loadFiles = (pageUrl, links, srcDirPath) => {
   });
 };
 
-export default (urlPage, outputDir) => {
+export default async (urlPage, outputDir) => {
   const pagePath = getPagePath(urlPage, outputDir);
   const srcDirPath = getSrcDirPath(urlPage, outputDir);
-  return axios.get(urlPage)
-    .then((response) => {
-      log(`request to ${urlPage}`);
-      return response.data;
-    })
-    .then((data) => {
-      log(`saving page in ${pagePath}`);
-      return fs.writeFile(pagePath, data, 'utf8');
-    })
-    .then(() => {
-      log(`creating directory '${srcDirPath}' for saving sources from the page'`);
-      return fs.mkdir(srcDirPath);
-    })
-    .then(() => fs.readFile(pagePath))
-    .then((pageFileData) => {
-      log('start downloading files to the local directory and changing links in the page to local sources');
-      const pageBody = pageFileData.toString();
-      const links = getSrcLinks(pageBody);
-      loadFiles(urlPage, links, srcDirPath);
-      return Promise.all([loadFiles(urlPage, links, srcDirPath),
-        fs.writeFile(pagePath, changeSrcLinks(pageBody, srcDirPath), 'utf8')]);
-    })
-    .then(() => `\nPage was downloaded as ${path.parse(pagePath).base}`)
-    .catch((error) => {
-      if (error.response) {
-        const msg = `Error. Request failed with status code ${error.response.status}`;
-        return Promise.reject(msg);
-      }
-      const errMessages = {
-        ENOTFOUND: 'Error. This address was not found',
-        ENOENT: "Error. The directory doesn't exists",
-        EACCES: "Error. You don't have permissons to write files to this directory",
-        EEXIST: 'Error. File with this name already exists',
-        ENOTDIR: "Error. Given path isn't path to the directory",
-      };
-      return Promise.reject(errMessages[error.code]);
-    });
+  try {
+    const response = await axios.get(urlPage);
+    log(`request to ${urlPage}`);
+    log(`saving page in ${pagePath}`);
+    await fs.writeFile(pagePath, response.data, 'utf8');
+    log(`creating directory '${srcDirPath}' for saving sources from the page'`);
+
+    await fs.mkdir(srcDirPath);
+    const pageFileData = await fs.readFile(pagePath);
+    log('start downloading files to the local directory and changing links in the page to local sources');
+
+    const pageBody = pageFileData.toString();
+    const links = getSrcLinks(pageBody);
+    loadFiles(urlPage, links, srcDirPath);
+    await Promise.all([loadFiles(urlPage, links, srcDirPath),
+      fs.writeFile(pagePath, changeSrcLinks(pageBody, srcDirPath), 'utf8')]);
+    const message = `\nPage was downloaded as ${path.parse(pagePath).base}`;
+    return message;
+  } catch (error) {
+    if (error.response) {
+      const msg = `Error. Request failed with status code ${error.response.status}`;
+      return Promise.reject(msg);
+    }
+    const errMessages = {
+      ENOTFOUND: 'Error. This address was not found',
+      ENOENT: "Error. The directory doesn't exists",
+      EACCES: "Error. You don't have permissons to write files to this directory",
+      EEXIST: 'Error. File with this name already exists',
+      ENOTDIR: "Error. Given path isn't path to the directory",
+    };
+    return Promise.reject(errMessages[error.code]);
+  }
 };
